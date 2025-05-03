1572b1b53d287b1ba40ab49c3ea58b62d4cfe256
Rafiputraaa <raafiputraa2@gmail.com>
2025-05-03 23:40:50 +0700

    setup and initialization

---
+++ frontend/app/page.js
@@ -0,0 +1,15 @@
+// frontend/pages/index.js
+"use client"
+import { use, useEffect, useState } from 'react';
+
+export default function Home() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return <h1>{message}</h1>;
+}
